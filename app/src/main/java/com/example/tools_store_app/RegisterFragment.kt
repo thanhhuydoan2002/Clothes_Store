9f314e92f2532d51ae100201109472ab17aec6f9
thanhhuydoan2002 <ddoanthanhhuy@gmail.com>
2023-08-13 09:54:20 +0700

    Like adapter updated

--- app/src/main/java/com/example/tools_store_app/RegisterFragment.kt
+++ app/src/main/java/com/example/tools_store_app/RegisterFragment.kt
@@ -1,4 +1,72 @@
 package com.example.tools_store_app
 
-class RegisterFragment {
+
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import com.example.tools_store_app.Extensions.toast
+import com.example.tools_store_app.databinding.RegisterFragmentBinding
+import com.google.firebase.auth.FirebaseAuth
+
+class SignUpFragment : Fragment(R.layout.register_fragment) {
+
+    private lateinit var binding: RegisterFragmentBinding
+    private lateinit var auth : FirebaseAuth
+
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding = RegisterFragmentBinding.bind(view)
+        auth = FirebaseAuth.getInstance()
+
+
+
+
+
+
+        binding.btnRegister.setOnClickListener {
+
+            if (
+                binding.etEmailRegister.text.isNotEmpty() &&
+                binding.etNameRegister.text.isNotEmpty() &&
+                binding.etPasswordRegister.text.isNotEmpty()
+            ) {
+
+                createUser(binding.etEmailRegister.text.toString(),binding.etNameRegister.text.toString())
+
+
+            }
+        }
+        binding.tvNavigateToLogin.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_registerFragment_to_loginFragment)
+        }
+
+
+    }
+
+    private fun createUser(email: String, password: String) {
+
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener {task ->
+                if(task.isSuccessful){
+                    requireActivity().toast("New User created")
+
+                    Navigation.findNavController(requireView())
+                        .navigate(R.id.action_registerFragment_to_mainFragment)
+
+                }
+                else{
+                    requireActivity().toast(task.exception!!.localizedMessage)
+                }
+
+            }
+
+    }
+
+
 }
--- app/src/main/java/com/example/tools_store_app/rvadapters/LikeAdapter.kt
+++ app/src/main/java/com/example/tools_store_app/rvadapters/LikeAdapter.kt
@@ -1,4 +1,69 @@
 package com.example.tools_store_app.rvadapters
 
-class LikeAdapter {
+import android.content.Context
+import android.content.res.ColorStateList
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.tools_store_app.Models.LikeModel
+import com.example.tools_store_app.databinding.MainDisplayItemBinding
+
+class LikeAdapter(
+    private val context: Context,
+    private val list: ArrayList<LikeModel>,
+    private val productClickInterface: LikedProductOnClickInterface,
+    private val likeClickInterface: LikedOnClickInterface,
+):RecyclerView.Adapter<LikeAdapter.ViewHolder>() {
+
+
+    inner class ViewHolder(val binding: MainDisplayItemBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(MainDisplayItemBinding.inflate(LayoutInflater.from(parent.context),
+            parent,
+            false))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val currentItem = list[position]
+        holder.binding.tvNameShoeDisplayItem.text = "${currentItem.brand} ${currentItem.name}"
+        holder.binding.tvPriceShoeDisplayItem.text = "VND${currentItem.price}"
+        holder.binding.btnLike.backgroundTintList = ColorStateList.valueOf(Color.RED)
+
+
+        Glide
+            .with(context)
+            .load(currentItem.imageUrl)
+            .into(holder.binding.ivShoeDisplayItem)
+
+
+        holder.itemView.setOnClickListener {
+            productClickInterface.onClickProduct(list[position])
+        }
+
+        holder.binding.btnLike.setOnClickListener {
+            likeClickInterface.onClickLike(currentItem)
+            holder.binding.btnLike.backgroundTintList = ColorStateList.valueOf(Color.WHITE)
+
+            likeClickInterface.onClickLike(currentItem)
+        }
+
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+
+}
+
+interface LikedProductOnClickInterface {
+    fun onClickProduct(item: LikeModel)
+}
+
+interface LikedOnClickInterface{
+    fun onClickLike(item: LikeModel)
 }

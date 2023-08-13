3c90b61e10a2b7d8be75ce5530c6c4a7616899bd
thanhhuydoan2002 <ddoanthanhhuy@gmail.com>
2023-08-13 11:44:51 +0700

    Like Fragment Updated

--- app/src/main/java/com/example/tools_store_app/LikeFragment.kt
+++ app/src/main/java/com/example/tools_store_app/LikeFragment.kt
@@ -1,4 +1,99 @@
 package com.example.tools_store_app
 
-class LikeFragment {
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.tools_store_app.databinding.LikedProductsFragmentBinding
+import com.example.tools_store_app.rvadapters.*
+import com.example.tools_store_app.Extensions.toast
+import com.example.tools_store_app.Models.LikeModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
+import com.google.firebase.ktx.Firebase
+
+class LikeFragment(): Fragment(R.layout.liked_products_fragment), LikedProductOnClickInterface,
+    LikedOnClickInterface {
+
+    private lateinit var binding: LikedProductsFragmentBinding
+    private lateinit var auth: FirebaseAuth
+    private lateinit var adapter: LikeAdapter
+    private lateinit var likedProductList: ArrayList<LikeModel>
+
+
+    private var likeDBRef = Firebase.firestore.collection("LikedProducts")
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding = LikedProductsFragmentBinding.bind(view)
+        auth = FirebaseAuth.getInstance()
+        likedProductList = ArrayList()
+        adapter = LikeAdapter(requireContext(),likedProductList,this,this)
+
+
+        binding.likeActualToolbar.setNavigationOnClickListener {
+            Navigation.findNavController(requireView()).popBackStack()
+        }
+
+
+        val productLayoutManager = GridLayoutManager(context, 2)
+        binding.rvLikedProducts.layoutManager = productLayoutManager
+        binding.rvLikedProducts.adapter = adapter
+
+
+        displayLikedProducts()
+
+    }
+
+
+    private fun displayLikedProducts() {
+
+        likeDBRef
+            .whereEqualTo("uid" , auth.currentUser!!.uid)
+            .get()
+            .addOnSuccessListener { querySnapshot ->
+                for (item in querySnapshot) {
+                    val likedProduct = item.toObject<LikeModel>()
+                    likedProductList.add(likedProduct)
+                    adapter.notifyDataSetChanged()
+                }
+
+            }
+            .addOnFailureListener{
+                requireActivity().toast(it.localizedMessage!!)
+            }
+    }
+
+    override fun onClickProduct(item: LikeModel) {
+
+    }
+
+    override fun onClickLike(item: LikeModel) {
+        //todo Remove from Liked Items
+
+        likeDBRef
+            .whereEqualTo("uid",auth.currentUser!!.uid)
+            .whereEqualTo("pid",item.pid)
+            .get()
+            .addOnSuccessListener { querySnapshot ->
+
+                for (item in querySnapshot){
+                    likeDBRef.document(item.id).delete()
+                    likedProductList.remove(item.toObject<LikeModel>())
+                    adapter.notifyDataSetChanged()
+                    requireActivity().toast("Bỏ yêu thích sản phẩm thành công")
+                }
+
+            }
+            .addOnFailureListener {
+                requireActivity().toast("Bỏ yêu thích sản phẩm thất bại, vui lòng thử lại sau")
+            }
+
+    }
+
 }
